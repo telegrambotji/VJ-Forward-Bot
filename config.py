870d83f33c3580d47ac5f7b3cc06afce62dfea80
telegrambotji <telegrambotji@gmail.com>
2025-08-15 18:20:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tutorialmaster22103:tutorialmaster22103@cluster0.pahxkfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
